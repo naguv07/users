85b32261cdf045cf9c083a1c6bebf2d4764459b2
Nageshwar Verma <nageshv07@gmail.com>
2020-10-24 21:29:02 +0530

    Updates

---
+++ precache-manifest.81cd4b28776baee5e69962f7f66da0a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2334732e13d52097967e89036b607c",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "80d3641c410405e87005",
+    "url": "/users/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "c681b5255826d8693197",
+    "url": "/users/static/css/main.42b609b7.chunk.css"
+  },
+  {
+    "revision": "80d3641c410405e87005",
+    "url": "/users/static/js/2.74530e6b.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/users/static/js/2.74530e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c681b5255826d8693197",
+    "url": "/users/static/js/main.557160de.chunk.js"
+  },
+  {
+    "revision": "a95b046da31513395b37",
+    "url": "/users/static/js/runtime-main.b37507d1.js"
+  }
+]);
